31a86020774daef839cc797a3d59238cd596178d
Abhiroop Santra <abhiroop.santra@gmail.com>
2024-02-21 11:29:14 +0400

    feat: Add game logic for player movement and collision detection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,92 @@
+use rusty_engine::prelude::*;
+
+const MOVEMENT_SPEED: f32 = 100.0;
+
+#[derive(Resource)]
+struct GameState {
+    high_score: u32,
+    current_score: u32,
+    enemy_labels: Vec<String>,
+    spawn_timer: Timer,
+}
+
+impl Default for GameState {
+    fn default() -> Self {
+        Self {
+            high_score: 0,
+            current_score: 0,
+            enemy_labels: Vec::new(),
+            spawn_timer: Timer::from_seconds(1.0, TimerMode::Once),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut game = Game::new();
+
+    let player = game.add_sprite("player", SpritePreset::RacingCarBlue);
+    player.translation = Vec2::new(0.0, 0.0);
+    player.rotation = SOUTH_WEST;
+    player.scale = 1.0;
+    player.collision = true;
+
+    let car1 = game.add_sprite("car1", SpritePreset::RacingCarYellow);
+    car1.translation = Vec2::new(300.0, 0.0);
+    car1.collision = true;
+
+    game.add_logic(game_logic);
+    game.run(GameState::default());
+}
+
+fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
+    // engine.show_colliders = true;
+
+    // handle collisions
+    for event in engine.collision_events.drain(..) {
+        // println!("{:#?}", event);
+        if event.state == CollisionState::Begin && event.pair.one_starts_with("player") {
+            // remove the sprite the player has collided with
+            for label in [event.pair.0, event.pair.1] {
+                if label != "player" {
+                    engine.sprites.remove(&label);
+                }
+            }
+
+            // increase the score
+            game_state.current_score += 1;
+            println!("Current score: {}", game_state.current_score);
+        }
+    }
+
+    // handle movement
+    let player = engine.sprites.get_mut("player").unwrap();
+    // player.translation.x += 100.0 * engine.delta_f32;
+
+    if engine
+        .keyboard_state
+        .pressed_any(&[KeyCode::Up, KeyCode::W])
+    {
+        player.translation.y += MOVEMENT_SPEED * engine.delta_f32;
+    }
+
+    if engine
+        .keyboard_state
+        .pressed_any(&[KeyCode::Down, KeyCode::S])
+    {
+        player.translation.y -= MOVEMENT_SPEED * engine.delta_f32;
+    }
+
+    if engine
+        .keyboard_state
+        .pressed_any(&[KeyCode::Right, KeyCode::D])
+    {
+        player.translation.x += MOVEMENT_SPEED * engine.delta_f32;
+    }
+
+    if engine
+        .keyboard_state
+        .pressed_any(&[KeyCode::Left, KeyCode::A])
+    {
+        player.translation.x -= MOVEMENT_SPEED * engine.delta_f32;
+    }
 }
